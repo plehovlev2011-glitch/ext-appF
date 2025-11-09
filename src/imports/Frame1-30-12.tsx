4520482e040515fcb99cceae4c672fbace4ea9f1
plehovlev2011-glitch <plehovlev2011@gmail.com>
2025-11-09 20:51:03 +0300

    Add files via upload

---
+++ src/imports/Frame1-30-12.tsx
@@ -0,0 +1,30 @@
+export default function Frame1() {
+  return (
+    <div className="css-j9f0op css-vf8mzy">
+      <div className="css-roiesn css-wyolll">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 431 241">
+            <ellipse cx="215.5" cy="120.5" fill="var(--fill-0, #FF0000)" id="Ellipse 1" rx="215.5" ry="120.5" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-f30xg8 css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 675 241">
+            <ellipse cx="337.5" cy="120.5" fill="var(--fill-0, #FFCACA)" id="Ellipse 2" rx="337.5" ry="120.5" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-i77hpp css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 631 241">
+            <ellipse cx="315.5" cy="120.5" fill="var(--fill-0, #FFCACA)" id="Ellipse 3" rx="315.5" ry="120.5" />
+          </svg>
+        </div>
+      </div>
+      <p className="css-974356 css-bs6e4b css-tzn6qh css-wvk7u6">дом</p>
+      <p className="css-bbr9ac css-bs6e4b css-tzn6qh css-ue4aiw">доставим</p>
+      <p className="css-bbr9ac css-bs6e4b css-tzn6qh css-v7skh8">торжества</p>
+    </div>
+  );
+}
---
+++ src/imports/Group1-43-43.tsx
@@ -0,0 +1,113 @@
+import svgPaths from "./svg-q47v0ktl3f";
+
+export default function Group1() {
+  return (
+    <div className="css-j9f0op css-vf8mzy">
+      <div className="css-61y1s7 css-bc1d1i css-j45cbg" />
+      <div className="css-bbmv0m css-dif40j css-ntqiz5" />
+      <div className="css-4ea44u css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-baulaq css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-ci5bdx css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-eq72tv css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-roiesn css-ufbs9f">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-6mbvks css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-d07lh9 css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-nky2wt css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-jsddlm css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-d07lh9 css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-nky2wt css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-jsddlm css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-4ggvy4 css-roiesn">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} id="Ellipse 1" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-roiesn css-w49het">
+        <div className="css-gs60ek css-roiesn css-wixxpz">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 500 212">
+            <path d={svgPaths.p2b9e5180} fill="var(--fill-0, white)" id="Ellipse 14" stroke="var(--stroke-0, white)" strokeWidth="7" />
+          </svg>
+        </div>
+      </div>
+      <div className="css-3yrxng css-g53zeq css-i56x7w css-roiesn">
+        <p className="css-8jchzs css-x78r3n">за каждую покупку от 250 рублей при предъявлении дисконтной карты получайте фишку в приложении.</p>
+        <p className="css-8zr56v css-x78r3n">накопите 10 фишек - и 11-ая покупка станет бесплатной</p>
+      </div>
+      <p className="css-168s0y css-ckykaz css-tzn6qh css-u7rz4k">✓</p>
+    </div>
+  );
+}
